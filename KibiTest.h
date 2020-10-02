a6860983bcec3014fe88af8ae45e2e0f278dc4fe
S. Tyler Berryhill <stberryhill@pop-os.localdomain>
2020-10-01 23:59:21 -0500

    Adding before and after function supports for each test.

--- KibiTest.h
+++ KibiTest.h
@@ -1,6 +1,6 @@
 /* 
  * A minimal, single-header C89 testing framework in under a Kibibyte.
- * Sean Tyler Berryhill
+ * S. Tyler Berryhill
  */
 
 #ifndef _KIBITEST_H
@@ -9,13 +9,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _kibi_currentTestHasFailures; /* Do not modify externally */
+void nop() {}
+
+int _kCurrentTestFailed; /* Don't modify externally */
+void (*_kBefore)() = nop;
+void (*_kAfter)() = nop;
 
 #define Kibi_Test(function) ({\
-  _kibi_currentTestHasFailures = 0;\
+  _kCurrentTestFailed = 0;\
   printf("%s:\n", #function);\
+  _kBefore();\
   function();\
-  if (_kibi_currentTestHasFailures) {\
+  _kAfter();\
+  if (_kCurrentTestFailed) {\
     printf("...FAILED.\n\n");\
     exit(EXIT_FAILURE);\
   } else {\
@@ -25,16 +31,21 @@ int _kibi_currentTestHasFailures; /* Do not modify externally */
 
 #define Kibi_AssertTrue(condition) ({\
   if (!(condition)) {\
-    _kibi_currentTestHasFailures = 1;\
-    printf("\tError @ %s:%d\texpected true but was false\n", __FILE__, __LINE__);\
+    _kCurrentTestFailed = 1;\
+    printf("\tError - Assert true failed @ %s:%d\n", __FILE__, __LINE__);\
   }\
 })
 
 #define Kibi_AssertFalse(condition) ({\
   if (condition) {\
-    _kibi_currentTestHasFailures = 1;\
-    printf("\tError @ %s:%d\texpected false but was true\n", __FILE__, __LINE__);\
+    _kCurrentTestFailed = 1;\
+    printf("\tError - Assert false failed @ %s:%d\n", __FILE__, __LINE__);\
   }\
+})
+
+#define Kibi_ForEach(before, after) ({\
+  _kBefore = before;\
+  _kAfter = after;\
 })
 
 #endif
